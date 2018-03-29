2ee481822de977b9e3913e3c16b613d48336f500
Lucas Russo <lerwys@gmail.com>
2018-03-29 14:11:31 -0300

    install-docker.sh: fix redundant download of docker-compose

--- install-docker.sh
+++ install-docker.sh
@@ -33,9 +33,9 @@ sudo chroot ${ROOTFS} bash -c "getent group docker || groupadd docker"
 sudo chroot ${ROOTFS} usermod -aG docker ${GENERIC_USER}
 
 # Install docker-compose
-sudo chroot ${ROOTFS} curl -L \
+sudo chroot ${ROOTFS} bash -c "[ -f /usr/local/bin/docker-compose ] || curl -L \
     https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` \
-    -o /usr/local/bin/docker-compose
+    -o /usr/local/bin/docker-compose"
 sudo chroot ${ROOTFS} chmod +x /usr/local/bin/docker-compose
 
 # Setup common Docker directories
