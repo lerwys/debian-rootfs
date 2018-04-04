2f641cd7521a4820d91f8dd8c660450e16eb6db7
Lucas Russo <lerwys@gmail.com>
2018-04-04 16:21:50 -0300

    create-homes.sh: fix readability in long commands

--- create-homes.sh
+++ create-homes.sh
@@ -32,26 +32,26 @@ DMM7510_INSTANCE=DCCT1
 IMAGE_VERSION=debian-9
 
 # Testing Image
-/usr/bin/docker pull \
+/usr/bin/docker pull \\\\
     dockerregistry.lnls-sirius.com.br/dmm7510-epics-ioc:\\\${IMAGE_VERSION}
 
-/usr/bin/docker create \
-    -v /opt/epics/startup/ioc/dmm7510-epics-ioc/iocBoot/iocdmm7510/autosave \
-    --name dmm7510-epics-ioc-\\\${DMM7510_INSTANCE}-volume \
-    dockerregistry.lnls-sirius.com.br/dmm7510-epics-ioc:\\\${IMAGE_VERSION} \
+/usr/bin/docker create \\\\
+    -v /opt/epics/startup/ioc/dmm7510-epics-ioc/iocBoot/iocdmm7510/autosave \\\\
+    --name dmm7510-epics-ioc-\\\${DMM7510_INSTANCE}-volume \\\\
+    dockerregistry.lnls-sirius.com.br/dmm7510-epics-ioc:\\\${IMAGE_VERSION} \\\\
     2>/dev/null || true
 
-/usr/bin/docker run \
-    --net host \
-    -t \
-    --rm \
-    --volumes-from dmm7510-epics-ioc-\\\${DMM7510_INSTANCE}-volume \
-    --name dmm7510-epics-ioc-\\\${DMM7510_INSTANCE} \
-    dockerregistry.lnls-sirius.com.br/dmm7510-epics-ioc:\\\${IMAGE_VERSION} \
-    -i 10.0.18.37 \
-    -p 5025 \
-    -d DCCT1 \
-    -P TEST: \
+/usr/bin/docker run \\\\
+    --net host \\\\
+    -t \\\\
+    --rm \\\\
+    --volumes-from dmm7510-epics-ioc-\\\${DMM7510_INSTANCE}-volume \\\\
+    --name dmm7510-epics-ioc-\\\${DMM7510_INSTANCE} \\\\
+    dockerregistry.lnls-sirius.com.br/dmm7510-epics-ioc:\\\${IMAGE_VERSION} \\\\
+    -i 10.0.18.37 \\\\
+    -p 5025 \\\\
+    -d DCCT1 \\\\
+    -P TEST: \\\\
     -R DCCT:
 EOF
 "
