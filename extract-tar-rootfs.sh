4ef2b17eb85af677ef2ee7281648d5187e087a99
Lucas Russo <lerwys@gmail.com>
2018-04-06 09:57:35 -0300

    extract-tar-rootfs.sh: add script to extract a generated rootfs

---
+++ extract-tar-rootfs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+. ${SCRIPTPATH}/env-vars.sh
+
+ROOTFS_EXTRACT=rootfs-extract
+
+bash -c "\
+    mkdir -p ${ROOTFS_EXTRACT} && \
+    cd ${ROOTFS_EXTRACT} && \
+    sudo tar -xpvzf ../rootfs.tar.gz
+"
