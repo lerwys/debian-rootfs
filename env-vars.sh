c857dd54bc2231c2dbe2b10713c7abf72e1cbfb9
Lucas Russo <lerwys@gmail.com>
2018-04-06 10:13:15 -0300

    env-vars.sh: move HOME definitions to global file

--- env-vars.sh
+++ env-vars.sh
@@ -1,4 +1,6 @@
-set -euxo pipefail
+#!/usr/bin/env bash
+
+set -ue
 
 # User modifiable
 DEBIAN_URL="http://ftp.us.debian.org/debian"
@@ -11,3 +13,14 @@ ROOTFS_IP="192.168.2.12"
 EXPORTDIR=exports
 TOPDIR=${SCRIPTPATH}/${EXPORTDIR}
 ROOTFS=${TOPDIR}/${ROOTFSREL}
+
+# Home variables
+HOMESREL=()
+HOMESREL+=("home/dell-r230-server-1")
+HOMESREL+=("home/dell-r230-server-2")
+
+# Prefix homes array
+HOMES=()
+for homes in "${HOMESREL[@]}"; do
+    HOMES+=(${TOPDIR}/${homes})
+done
