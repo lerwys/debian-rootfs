d6253864e6b7736f78e1fd11a8ba52075910a101
Lucas Russo <lerwys@gmail.com>
2018-04-10 16:09:50 -0300

    create-homes.sh: remove  unnecessary compose generation
    This is already taken care by each specific home
    script

--- create-homes.sh
+++ create-homes.sh
@@ -13,6 +13,10 @@ for home in "${HOMES[@]}"; do
     mkdir -p ${home}
 done
 
+################################################
+### Configure start/stop scripts
+###############################################
+
 # Configure start pre
 for home in "${HOMES[@]}"; do
     sudo bash -c "cat << "EOF" > ${home}/bootstrap-start-pre-apps.sh
@@ -32,39 +36,6 @@ EOF
 "
 
     sudo chmod +x ${home}/bootstrap-start-post-apps.sh
-done
-
-# Configure env-files
-for home in "${HOMES[@]}"; do
-    sudo bash -c "cat << "EOF" > ${home}/00-dmm7510-epics-ioc.env
-IMAGE_VERSION=debian-9
-DMM7510_INSTANCE=DCCT1
-EOF
-"
-done
-
-# Configure homes
-for home in "${HOMES[@]}"; do
-    sudo bash -c "cat << "EOF" > ${home}/00-dmm7510-epics-ioc.yml
-version: '3'
-
-services:
-  dmm7510-epics-ioc:
-    image: dockerregistry.lnls-sirius.com.br/dmm7510-epics-ioc:\\\${IMAGE_VERSION}
-    env_file:
-      - 00-dmm7510-epics-ioc.env
-    container_name: dmm7510-epics-ioc-dcct-1
-    command: -i 10.0.18.37 -p 5025 -d \\\${DMM7510_INSTANCE} -P TEST: -R DCCT:
-    volumes:
-      - type: bind
-        source: ${EPICS_AUTOSAVE}
-        target: /opt/epics/startup/ioc/dmm7510-epics-ioc/iocBoot/iocdmm7510/autosave
-    network_mode: \"host\"
-
-volumes:
-  dmm7510-autosave-volume:
-EOF
-"
 done
 
 # Configure stop pre
