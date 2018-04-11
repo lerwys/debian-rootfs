3ef1f2b476f0642d288a92d021a85fcc19b9f8ac
Lucas Russo <lerwys@gmail.com>
2018-04-11 10:16:13 -0300

    install-docker.sh: fix wrong distro/flavor in docker install
    The commands were being executed on the host,
    so the distro/flavor were been set wrongly
    to the host version and not the target ones.

--- install-docker.sh
+++ install-docker.sh
@@ -18,16 +18,17 @@ sudo chroot ${ROOTFS} apt-get install -y \
     lvm2
 
 sudo chroot ${ROOTFS} bash -c \
-    "curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -"
-sudo chroot ${ROOTFS} add-apt-repository \
+    'curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -'
+sudo chroot ${ROOTFS} bash -c \
+    'add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
     $(lsb_release -cs) \
-    stable"
+    stable"'
 
 # Install Docker package
 sudo chroot ${ROOTFS} apt-get update
 sudo chroot ${ROOTFS} apt-get install -y \
-    docker-ce=${DOCKER_VERSION}.ce
+    docker-ce=${DOCKER_VERSION}${DOCKER_VERSION_DEBIAN_SUFFIX}
 
 # Add GENERIC_USER to docker group
 sudo chroot ${ROOTFS} bash -c "getent group docker || groupadd docker"
