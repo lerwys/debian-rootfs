92f535d65e48937cf75bae25b2e90e06ea0d2450
Lucas Russo <lerwys@gmail.com>
2018-04-17 09:58:14 -0300

    create-rootfs.sh: add EPICS broadcast iptables rule on boot

--- create-rootfs.sh
+++ create-rootfs.sh
@@ -79,6 +79,55 @@ EOF
     "
 done
 
+# Create special policy for broadcasting special packets. This is important
+# to EPICS search requests, as multiple IOCs might be running on each host
+sudo bash -c "cat << "EOF" > ${ROOTFS}/etc/NetworkManager/dispatcher.d/22-epicsbcast
+#!/bin/sh -e
+# Called when an interface goes up / down
+
+# Author: Ralph Lange <Ralph.Lange@gmx.de>
+
+# Make any incoming Channel Access name resolution queries go to the broadcast address
+# (to hit all IOCs on this host)
+
+# Change this if you run CA on a non-standard port
+PORT=5064
+
+IFACE=\\\$1
+MODE=\\\$2
+
+[ \"\\\$IFACE\" != \"lo\" ] || exit 0
+
+line=\\\`/sbin/ifconfig \\\$IFACE | grep \"inet \"\\\`
+
+# Fedora ifconfig output
+addr=\\\`echo \\\$line | sed -e 's/.*inet \([0-9.]*\).*/\1/'\\\`
+bcast=\\\`echo \\\$line | sed -e 's/.*broadcast \([0-9.]*\).*/\1/'\\\`
+
+if [ -z \"\\\$addr\" -o -z \"\\\$bcast\" ]
+then
+    # RHEL ifconfig output
+    addr=\\\`echo \\\$line | sed -e 's/.*inet addr:\([0-9.]*\).*/\1/'\\\`
+    bcast=\\\`echo \\\$line | sed -e 's/.*Bcast:\([0-9.]*\).*/\1/'\\\`
+fi
+
+[ -z \"\\\$addr\" -o -z \"\\\$bcast\" ] && return 1
+
+if [ \"\\\$MODE\" = \"up\" ]
+then
+    /sbin/iptables -t nat -A PREROUTING -d \\\$addr -p udp --dport \\\$PORT -j DNAT --to-destination \\\$bcast
+elif [ \"\\\$MODE\" = \"down\" ]
+then
+    /sbin/iptables -t nat -D PREROUTING -d \\\$addr -p udp --dport \\\$PORT -j DNAT --to-destination \\\$bcast
+fi
+
+exit 0
+EOF
+"
+
+sudo chmod +x ${ROOTFS}/etc/NetworkManager/dispatcher.d/22-epicsbcast
+
+###############################################################################
 # Create .rw folders to mount as overlay
 sudo mkdir -p ${ROOTFS}/var/lib/nfs.rw
 sudo mkdir -p ${ROOTFS}/etc/docker.rw
