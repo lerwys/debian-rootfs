067aeac3cd4d7121e715eccb4dc5de8dd032da7a
Lucas Russo <lerwys@gmail.com>
2018-04-18 17:57:42 -0300

    homes/dell-r230-server-1/*: add SINAP Timing IOCs

--- homes/dell-r230-server-1/create-home.sh
+++ homes/dell-r230-server-1/create-home.sh
@@ -86,3 +86,120 @@ services:
     network_mode: \"host\"
 EOF
 "
+
+###############################################################################
+# Configure compose IOC #3
+###############################################################################
+
+mkdir -p ${HOMEDIR}/03-sinap-timing-epics-ioc
+
+# Configure env-files
+bash -c "cat << "EOF" > ${HOMEDIR}/03-sinap-timing-epics-ioc/.env
+IMAGE_VERSION=0.1.0-debian-9.2
+SINAP_TIMING_INSTANCE=EVG1
+SINAP_TIMING_IP=10.2.117.35
+SINAP_TIMING_PORT=50111
+SINAP_TIMING_AREA_PREFIX=AS-Glob:
+SINAP_TIMING_DEVICE_PREFIX=TI-EVG:
+EOF
+"
+
+# Configure homes
+bash -c "cat << "EOF" > ${HOMEDIR}/03-sinap-timing-epics-ioc/00-sinap-timing-epics-ioc.yml
+version: '3.4'
+
+services:
+  sinap-timing-epics-ioc:
+    image: dockerregistry.lnls-sirius.com.br/sinap-timing-epics-ioc:\\\${IMAGE_VERSION}
+    container_name: sinap-timing-epics-ioc-\\\${SINAP_TIMING_INSTANCE}
+    command: \"\\\\
+        -i \\\${SINAP_TIMING_IP} \\\\
+        -p \\\${SINAP_TIMING_PORT} \\\\
+        -d \\\${SINAP_TIMING_INSTANCE} \\\\
+        -P \\\${SINAP_TIMING_AREA_PREFIX} \\\\
+        -R \\\${SINAP_TIMING_DEVICE_PREFIX}\"
+    volumes:
+      - type: bind
+        source: ${EPICSAUTOSAVE}
+        target: /opt/epics/startup/ioc/sinap-timing-epics-ioc/iocBoot/ioctiming/autosave
+    network_mode: \"host\"
+EOF
+"
+
+###############################################################################
+# Configure compose IOC #4
+###############################################################################
+
+mkdir -p ${HOMEDIR}/04-sinap-timing-epics-ioc
+
+# Configure env-files
+bash -c "cat << "EOF" > ${HOMEDIR}/04-sinap-timing-epics-ioc/.env
+IMAGE_VERSION=0.1.0-debian-9.2
+SINAP_TIMING_INSTANCE=EVE1
+SINAP_TIMING_IP=10.2.117.34
+SINAP_TIMING_PORT=50124
+SINAP_TIMING_AREA_PREFIX=AS-Glob:
+SINAP_TIMING_DEVICE_PREFIX=TI-EVE-1:
+EOF
+"
+
+# Configure homes
+bash -c "cat << "EOF" > ${HOMEDIR}/04-sinap-timing-epics-ioc/00-sinap-timing-epics-ioc.yml
+version: '3.4'
+
+services:
+  sinap-timing-epics-ioc:
+    image: dockerregistry.lnls-sirius.com.br/sinap-timing-epics-ioc:\\\${IMAGE_VERSION}
+    container_name: sinap-timing-epics-ioc-\\\${SINAP_TIMING_INSTANCE}
+    command: \"\\\\
+        -i \\\${SINAP_TIMING_IP} \\\\
+        -p \\\${SINAP_TIMING_PORT} \\\\
+        -d \\\${SINAP_TIMING_INSTANCE} \\\\
+        -P \\\${SINAP_TIMING_AREA_PREFIX} \\\\
+        -R \\\${SINAP_TIMING_DEVICE_PREFIX}\"
+    volumes:
+      - type: bind
+        source: ${EPICSAUTOSAVE}
+        target: /opt/epics/startup/ioc/sinap-timing-epics-ioc/iocBoot/ioctiming/autosave
+    network_mode: \"host\"
+EOF
+"
+
+###############################################################################
+# Configure compose IOC #4
+###############################################################################
+
+mkdir -p ${HOMEDIR}/05-sinap-timing-epics-ioc
+
+# Configure env-files
+bash -c "cat << "EOF" > ${HOMEDIR}/05-sinap-timing-epics-ioc/.env
+IMAGE_VERSION=0.1.0-debian-9.2
+SINAP_TIMING_INSTANCE=EVR1
+SINAP_TIMING_IP=10.2.117.37
+SINAP_TIMING_PORT=50125
+SINAP_TIMING_AREA_PREFIX=AS-Glob:
+SINAP_TIMING_DEVICE_PREFIX=TI-EVE-1:
+EOF
+"
+
+# Configure homes
+bash -c "cat << "EOF" > ${HOMEDIR}/05-sinap-timing-epics-ioc/00-sinap-timing-epics-ioc.yml
+version: '3.4'
+
+services:
+  sinap-timing-epics-ioc:
+    image: dockerregistry.lnls-sirius.com.br/sinap-timing-epics-ioc:\\\${IMAGE_VERSION}
+    container_name: sinap-timing-epics-ioc-\\\${SINAP_TIMING_INSTANCE}
+    command: \"\\\\
+        -i \\\${SINAP_TIMING_IP} \\\\
+        -p \\\${SINAP_TIMING_PORT} \\\\
+        -d \\\${SINAP_TIMING_INSTANCE} \\\\
+        -P \\\${SINAP_TIMING_AREA_PREFIX} \\\\
+        -R \\\${SINAP_TIMING_DEVICE_PREFIX}\"
+    volumes:
+      - type: bind
+        source: ${EPICSAUTOSAVE}
+        target: /opt/epics/startup/ioc/sinap-timing-epics-ioc/iocBoot/ioctiming/autosave
+    network_mode: \"host\"
+EOF
+"
